f3d2787a3af924d5a07f7044386c3ec468d6b511
elmiraabolfazli <efabolfazli@gmail.com>
2023-06-05 10:09:28 +0330

    3 vue

--- bazargam/src/vue/vue.js
+++ bazargam/src/vue/vue.js
@@ -2,14 +2,23 @@ const  app =Vue.createApp({
     //deta,function,template 
     data(){
         return{
+           url: "https://www.google.com",
+           x:0,
+           y:0,
            title:"boof koor",
            author:"Elmira",
            age:100,
-           show:true
-        }
+           show:true,
+           books:[
+            {title:"at",author:"au",Image:"1.jpeg",isfav:false},
+            {title:"bt",author:"bu",Image:"2.jpeg",isfav:true},
+            {title:"ct",author:"cu",Image:"3.jpeg",isfav:true},
+           ]
+         }           
     }, 
+   
     
-        methods : {
+      methods : {
             dec(){
                 this.age--         
              },
@@ -19,6 +28,17 @@ const  app =Vue.createApp({
              },
              hide(){
                 this.show=!this.show
+             },
+             handleEvent(e){
+               console.log(e);
+             },
+             handleMouseMove(e){
+               this.x= e.offsetX
+               this.y= e.offsetY
+             },
+             Cang(book){
+              book.isfav=!book.isfav
+              
              }
 
         }
