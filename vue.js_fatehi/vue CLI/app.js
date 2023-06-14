e6be11a3a2998770e0398140888ee1d5afb086df
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-06-14 21:37:29 +0330

    starting vue cli

---
+++ vue.js_fatehi/vue CLI/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    template: "",
+
+    data() {
+        return {
+            
+        }
+    },
+
+    methods: {
+        
+    },
+
+    computed: {
+        
+    },
+});
+
+app1.mount("#app");
