2cd12fbd5955181b5aae545cb0df8b4001e059fc
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-04 19:17:41 +0330

    different types of styling for specific and global tags of the components

--- vue.js_fatehi/vue CLI/my-vue-project/src/components/Modal.vue
+++ vue.js_fatehi/vue CLI/my-vue-project/src/components/Modal.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="backdrop">
         <div class="modal">
+            <h1> Modal </h1>
             <p> modal context </p>
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>
     .modal {
         width: 400px;
         margin: 100px auto;
@@ -21,5 +22,11 @@
         background: rgba(0, 0, 0, 0.5);
         width: 100%;
         height: 100%;
+    }
+
+    .modal h1 {
+        color: aquamarine;
+        border: none;
+        padding: 0;
     }
 </style>
