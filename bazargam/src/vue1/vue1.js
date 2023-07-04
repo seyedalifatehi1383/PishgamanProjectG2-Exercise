4a46ab96ab37d64a248fc10e45379bef6279d891
elmiraabolfazli <efabolfazli@gmail.com>
2023-07-04 09:15:23 +0330

    vue3

---
+++ bazargam/src/vue1/vue1.js
@@ -0,0 +1,8 @@
+//{{}}
+//v-on:tipe(click)="fun" =>@click=""
+//v-if=     =javas     :vue   v-show=
+// v-else
+//@mouseover="" @mouseleave="" @dblclick=""  @mousemove=""
+// v-for="item in array"
+// v-bind:hrfe="link"     let link="hhtps....."  -> :hrfe=""
+//:class="{fun}"
