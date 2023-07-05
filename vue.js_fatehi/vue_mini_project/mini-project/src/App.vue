8ff45bd4410c4c2f14cc0860b932d90e38ab963a
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-05 15:45:21 +0330

    creating the modal of the mini project

--- vue.js_fatehi/vue_mini_project/mini-project/src/App.vue
+++ vue.js_fatehi/vue_mini_project/mini-project/src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1> OpenCode Reaction Timer </h1>
+    <button @click="start" :disabled="isPlaying"> Play </button>
+    <Block v-if="isPlaying" :delay="delay"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Block},
+  data() {
+    return {
+      delay: null,
+      isPlaying: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    }
   }
 }
 </script>
@@ -20,7 +34,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
