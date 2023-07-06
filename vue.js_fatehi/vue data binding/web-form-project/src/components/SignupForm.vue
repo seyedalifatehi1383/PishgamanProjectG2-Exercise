39a00e60a34d2c3e21ba7b365b2ae83e29258854
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-06 09:17:38 +0330

    start creating the form

---
+++ vue.js_fatehi/vue data binding/web-form-project/src/components/SignupForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <form>
+        <label> Email </label>
+        <input type="email" required>
+    </form>
+</template>    
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
