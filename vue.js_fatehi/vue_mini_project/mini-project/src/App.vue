0da4128fa6cd6165ac19cd3e391b01307a5e9e0f
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-06 09:02:52 +0330

    completing the mini project

--- vue.js_fatehi/vue_mini_project/mini-project/src/App.vue
+++ vue.js_fatehi/vue_mini_project/mini-project/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container">
     <h1> OpenCode Reaction Timer </h1>
+import Results from './components/Results.vue';
     <button @click="start" :disabled="isPlaying"> Play </button>
     <p v-if="showResult"> Reaction: {{ score }} ms </p>
     <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+    <Results v-if="showResult" :score="score"/>
   </div>
 </template>
 
 <script>
 import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 
 export default {
   name: 'App',
-  components: {Block},
+  components: { Block, Results },
   data() {
     return {
       delay: null,
@@ -46,5 +49,22 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button [disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ vue.js_fatehi/vue_mini_project/mini-project/src/components/Results.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="resultContainer">
+        <p v-if="showResult"> Reaction: {{ score }} ms </p>
+        <p class="rank"> {{ rank }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+
+    mounted() {
+        if(this.score < 250) {
+            this.rank = 'عالی بود عالی'
+        }
+
+        else if(this.score < 400) {
+            this.rank = 'هی بدک نبود'
+        }
+
+        else {
+            this.rank = 'حلزون کی بودی تو'
+        }
+    }
+}
+</script>
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color: #0faf78;
+    font-weight: bold;
+}
+</style>
