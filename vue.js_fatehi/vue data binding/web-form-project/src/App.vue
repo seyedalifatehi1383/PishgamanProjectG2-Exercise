614df9b4d15362913312d4debb2cf29bf0efc585
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-06 09:13:14 +0330

    starting data binding

---
+++ vue.js_fatehi/vue data binding/web-form-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
