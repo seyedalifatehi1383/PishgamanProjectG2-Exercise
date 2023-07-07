61a1683fe499726d49dbfd6eec0f39773c5223fe
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-07 10:15:57 +0330

    do...while loop in js

--- app_fatehi.js
+++ app_fatehi.js
@@ -277,3 +277,20 @@ while (i <= 5) {
     if (i % 2 !== 0) console.log(i)
     i++;
 }
+
+// do...while
+i = 1
+do {
+    if (i % 2 !== 0) console.log(i)
+    i++;
+} while(i <= 5)
+
+i = 9
+do {
+    if (i % 2 !== 0) console.log(i)
+    i++;
+} while(i <= 5)
+// executes one time
+// while loop doesnt execute
+
+// for...in
