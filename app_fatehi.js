e75315d102abbc6ff752c0a5c4d59e51337c25bf
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-07 10:01:40 +0330

    while loop in js

--- app_fatehi.js
+++ app_fatehi.js
@@ -266,9 +266,14 @@ else console.log('Unknown User')
 
 // Loops
 // for
-
+ 
 for (let i = 1; i <= 5; i++) {
     if (i % 2 !== 0) console.log(i)
 }
 
 // while
+let i = 1;
+while (i <= 5) {
+    if (i % 2 !== 0) console.log(i)
+    i++;
+}
