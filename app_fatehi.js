87d0de63ce916e1f6939ecd03ffba37d189d93f3
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-07 13:58:14 +0330

    creating sum of the multiples 3 and 5 function in js

--- app_fatehi.js
+++ app_fatehi.js
@@ -468,4 +468,17 @@ function showProperties(obj) {
 }
 
 // sum of the multiples 3 and 5
-function
+console.log(sum(10)) // 33 (test)
+// multiples of 3 : 3, 6, 9
+// multiples of 5 : 5, 10
+
+function sum(limit) {
+    let sum = 0;
+
+    for (let i = 1; i <= limit; i++) {
+        if (i % 3 === 0 || i % 5 === 0)
+            sum += i
+    }
+
+    return sum
+}
