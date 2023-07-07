5c316853abbddee013ea38189debf5f802990f62
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-07 14:16:37 +0330

    creating grade mark function in js

--- app_fatehi.js
+++ app_fatehi.js
@@ -482,3 +482,34 @@ function sum(limit) {
 
     return sum
 }
+
+// grade
+const marks = [80, 80, 50]
+// average = 70 = C
+// 1-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+console.log(calculateGrade(marks)) // test
+
+function calculateGrade(marks) {
+    const avg = calculateAverage(marks)
+
+    if (avg < 60) return 'F'
+    if (avg < 70) return 'D'
+    if (avg < 80) return 'C'
+    if (avg < 90) return 'B'
+    return 'A'
+}
+
+function calculateAverage(array) {
+    let sum = 0
+    
+    for (let value of array) {
+        sum += value;
+    }
+
+    return sum / array.length;
+}
