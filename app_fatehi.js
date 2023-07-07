fe50e284bb8d423118842a56be0f2af0175bb42f
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-07 09:46:11 +0330

    switch...case in js

--- app_fatehi.js
+++ app_fatehi.js
@@ -243,5 +243,23 @@ else if (hour >= 12 && hour < 18)
 else
     console.log('good evening')
 
-
 // 2- switch...case
+
+let role = 'guest'
+
+switch (role) {
+    case 'guest':
+        console.log('Guest User')
+        break;
+
+    case 'moderator':
+        console.log('Moderator User')
+        break;
+
+    default:
+        console.log('Unknown User')
+}
+
+if (role === 'guest') console.log('Guest User')
+else if (role === 'moderator') console.log('Moderator User')
+else console.log('Unknown User')
