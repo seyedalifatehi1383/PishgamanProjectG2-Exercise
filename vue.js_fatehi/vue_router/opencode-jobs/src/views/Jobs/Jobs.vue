c6ffb703e9ba88c7f34cb946184180bb0316ad0c
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-09 00:28:59 +0330

    finish learning vue

--- vue.js_fatehi/vue_router/opencode-jobs/src/views/Jobs/Jobs.vue
+++ vue.js_fatehi/vue_router/opencode-jobs/src/views/Jobs/Jobs.vue
@@ -3,7 +3,7 @@
     <div v-if="jobs.length">
         <div v-for="job in jobs" :key="job.id" class="job">
             <router-link :to="{name : 'JobsDetails', params: {id: job.id}}">
-                <h2> {{ job.title }} </h2>
+                <h2> {{ job.title }} </h2> 
             </router-link>
         </div>
     </div>
