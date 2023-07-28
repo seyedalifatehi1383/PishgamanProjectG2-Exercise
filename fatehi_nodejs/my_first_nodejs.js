cc80c9c720d2f39ed0f358e044848fe640e8f7b0
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-28 10:04:28 +0330

    nodejs modules

--- fatehi_nodejs/my_first_nodejs.js
+++ fatehi_nodejs/my_first_nodejs.js
@@ -1,7 +1,6 @@
-var http = require('http')
+var http = require('http') // including an HTTP module
 
-http.createServer(
-    function (req, res) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('Hello, world');
-    }).listen(8080);
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello, world');
+}).listen(8080);
---
+++ fatehi_nodejs/myfirstmodule.js
@@ -0,0 +1,5 @@
+exports.myDateTime = function() {
+    return Date();
+}
+
+// "exports" keyword is for making props and methods available outside the module file
