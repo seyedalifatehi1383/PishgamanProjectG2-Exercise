e3d384deb6b623905d2698998db32660dec3ba3d
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-29 11:57:06 +0330

    creating database and table and using it in mysql

--- fatehi_nodejs/demo_db_connection.js
+++ fatehi_nodejs/demo_db_connection.js
@@ -2,15 +2,21 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "afatehi07@gmail.com",
-  password: "Alif@tehi_1383"
+  user: "safatehi",
+  password: "alifatehi1383",
+  database: "ToDo"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log('result: ' + result)
-  });
+  // con.query("SELECT * FROM Test WHERE 1", (err, result) => {
+  //   if (err) throw err;
+  //   console.log('result: ' + result)
+  // });
+
+  con.query("SELECT * FROM Test WHERE 1" , (err , result) =>{
+    console.log(result);
+    console.log(result[0].lName + ' ' + result[0].fName)
+  })
 });
