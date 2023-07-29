beebc31bf9f5a155b48851b8ae4c3a15b7a594ec
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-29 08:17:33 +0330

    install nodejs packages with npm

---
+++ fatehi_nodejs/demo_uppercase.js
@@ -0,0 +1,8 @@
+var http = require('http')
+var uc = require('upper-case')
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(uc.upperCase("hello world!"))
+    res.end()
+}).listen(8080);
