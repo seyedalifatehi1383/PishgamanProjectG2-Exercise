f84a691aff293a7de3222c10a0a9849ec97361c8
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-29 08:32:41 +0330

    nodejs events

---
+++ fatehi_nodejs/demo_rs_event.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+
+var readStream = fs.createReadStream('./demofile.txt');
+
+/*Write to the console when the file is opened:*/
+readStream.on('open', function () {
+  console.log('The file is open');
+});
