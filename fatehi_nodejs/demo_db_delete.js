80743154e5ef188157f76acdffa9c77f53a8d41a
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-30 11:19:23 +0330

    mysql delete statement

---
+++ fatehi_nodejs/demo_db_delete.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "safatehi",
+  password: "alifatehi1383",
+  database: "ToDo"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  //Delete all customers with the address "Mountain 21":
+  var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+    console.log(result)
+  });
+});
