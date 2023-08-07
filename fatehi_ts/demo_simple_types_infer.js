0e7010782fa0a8cefb6b50cadfce3cf9a394d8d5
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-08-07 08:58:23 +0330

    generic function in ts

--- fatehi_ts/demo_simple_types_infer.js
+++ fatehi_ts/demo_simple_types_infer.js
@@ -1,15 +1,12 @@
 // Implicit any as JSON.parse doesn't know what type of data it returns so it can be "any" thing... 
-const json = JSON.parse("55");
-
+var json = JSON.parse("55");
 // Most expect json to be an object, but it can be a string or a number like this example
 console.log(typeof json);
-
-function LoggerAndReturn<T>(thing: T) : T {
+function LoggerAndReturn(thing) {
     return thing;
 }
-
 // const message: string = LoggerAndReturn<string>('hello, world')
-const message: string = LoggerAndReturn('hello, world')
-const message2: number = LoggerAndReturn(2)
+var message = LoggerAndReturn('hello, world');
+var message2 = LoggerAndReturn(2);
 console.log(message);
 console.log(message2);
